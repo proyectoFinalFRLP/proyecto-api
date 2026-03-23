572ad79efc67ed61b56f9e5284e72f8e649cdfb9
Tomas Martin <tomassmartin04@gmail.com>
2026-03-23 20:58:37 -0300

    feat: initial setup

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users
+
+  get "up" => "rails/health#show", as: :rails_health_check
+
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: [:create, :show]
+    end
+  end
+end
